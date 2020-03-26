0b278eb3360ab1079195069fc656f5458c8759cb
Wilson Neto <wilsonpsn@hotmail.com>
2020-03-26 17:56:26 -0300

    Movido a logo

--- mobile/App.js
+++ mobile/App.js
@@ -1,22 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View>
       <Text>ODadada!</Text>
-      <Text>ODadada!</Text>
-      <Text>ODadada!</Text>
+
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    
-    flex: 1,
-    backgroundColor: '#7159c1',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
