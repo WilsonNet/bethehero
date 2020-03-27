9cc8cb25f83ad1c729eaf04afcb07f212142e277
Wilson Neto <wilsonpsn@hotmail.com>
2020-03-26 23:03:56 -0300

    Adicionado celular e email

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,15 +1,28 @@
 import React from 'react';
-import { View, Image, TouchableOpacity, Text } from 'react-native';
+import { View, Image, TouchableOpacity, Text, Linking } from 'react-native';
 import { useNav, useNavigation } from '@react-navigation/native';
 import { Feather } from '@expo/vector-icons';
 import styles from './styles';
 import logoImg from '../../assets/logo.png';
+import * as MailComposer from 'expo-mail-composer';
 
 const Detail = () => {
   const navigation = useNavigation();
   const navigateBack = () => {
     navigation.goBack();
   };
+  const message = 'Olá APAD, estou entrando em contato pois gostaria de ajudar no caso "Cadelinha ATtropelada" com o valor de R$120,00 '
+  const sendEmail = () => {
+    MailComposer.composeAsync({
+      subjet: 'Herói do caso: Cadelinha atropelada',
+      recipients: ['diego@rocketseat.com.br'],
+      body: message
+    });
+  };
+  const sendWhatsApp = () => {
+    Linking.openURL(`whatsapp://send?phone=+668357&text=${message}`)
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -40,10 +53,10 @@ const Detail = () => {
         <Text style={styles.heroTitle}>Seja o herói desse caso!</Text>
         <Text style={styles.heroDescription}>Entre em contat:</Text>
         <View style={styles.actions}>
-          <TouchableOpacity style={styles.action} onPress={() => {}}>
+          <TouchableOpacity style={styles.action} onPress={sendWhatsApp}>
             <Text style={styles.actionText}>WhatsApp</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.action} onPress={() => {}}>
+          <TouchableOpacity style={styles.action} onPress={sendEmail}>
             <Text style={styles.actionText}>Email</Text>
           </TouchableOpacity>
         </View>
