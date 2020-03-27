cc55750e12f91151da7eae093380870e990702e5
Wilson Neto <wilsonpsn@hotmail.com>
2020-03-26 21:02:25 -0300

    Estilizado o styles

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -10,8 +10,14 @@ const Incidents = () => {
     <View style={styles.container}>
       <View style={styles.header}>
         <Image source={logoImg} />
-        <Text style={styles.headerText} />
+        <Text style={styles.headerText}>
+          Total de <Text styles={styles.headerTextBold}>0 casos.</Text>
+        </Text>
       </View>
+      <Text style={styles.title}>Bem-vindo!</Text>
+      <Text style={styles.description}>
+        Escolha um dos casos abaixo e salve o dia.
+      </Text>
     </View>
   );
 };
