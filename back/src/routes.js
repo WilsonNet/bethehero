2a290ea7c1bc78c9593fddc8abb8f95aeb81c2fb
Wilson Neto <wilsonpsn@hotmail.com>
2020-03-27 17:21:22 -0300

    Adicionado validação

--- back/src/routes.js
+++ back/src/routes.js
@@ -6,6 +6,8 @@ const IncidentsController = require('./controllers/IncidentController');
 const ProfileController = require('./controllers/ProfileController');
 const SessionController = require('./controllers/SessionController');
 
+const { celebrate, Segments, Joi } = require('celebrate');
+
 const routes = express.Router();
 
 /**
@@ -14,17 +16,59 @@ const routes = express.Router();
  * Body - Corpo da req, criar ou alterar
  */
 
-
-routes.post('/sessions', SessionController.create)
+routes.post('/sessions', SessionController.create);
 
 routes.get('/ongs', OngController.index);
 
-routes.post('/ongs', OngController.create);
+routes.post(
+  '/ongs',
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string()
+        .required()
+        .email(),
+      whatsapp: Joi.string()
+        .min(10)
+        .max(11)
+        .required(),
+      city: Joi.string().required(),
+      uf: Joi.string()
+        .required()
+        .length(2)
+    })
+  }),
+  OngController.create
+);
 
-routes.get('/profile', ProfileController.index);
+routes.get(
+  '/profile',
+  celebrate({
+    [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required()
+    }).unknown()
+  }),
+  ProfileController.index
+);
 
 routes.post('/incidents', IncidentsController.create);
-routes.get('/incidents', IncidentsController.index);
-routes.delete('/incidents/:id', IncidentsController.delete);
+routes.get(
+  '/incidents',
+  celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+      page: Joi.number()
+    })
+  }),
+  IncidentsController.index
+);
+routes.delete(
+  '/incidents/:id',
+  celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.number().required()
+    })
+  }),
+  IncidentsController.delete
+);
 
 module.exports = routes;
